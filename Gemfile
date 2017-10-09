d8ecde9c0bce7fcb7b15f323413db95af1c2fc56
Jampa Thinlay <jthinlay@hotmail.com>
2017-10-09 17:53:34 +0000

    modified pins

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'devise', '~> 4.3'
+gem 'paperclip', '~> 5.1'
 # gem 'bcrypt', '~> 3.1.7'
 # gem 'unicorn'
 # Use Capistrano for deployment
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:eidt, :update, :destroy]
+  before_action :current_user, only: [:eidt, :update, :destroy]
 
   # GET /pins
   # GET /pins.json
@@ -71,6 +71,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
