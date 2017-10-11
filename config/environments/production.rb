7fd0a4793f3a9220b730dcf7d87d06195ca87de0
Jampa Thinlay <jthinlay@hotmail.com>
2017-10-11 00:56:36 +0000

    delete access keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,10 +84,15 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    # bucket: ENV.fetch('S3_BUCKET_NAME'),
+    # access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    # secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    # s3_region: ENV.fetch('AWS_REGION'),
+    
+     bucket: ENV.fetch('golden41'),
+     access_key_id: ENV.fetch('AKIAIHBSAKDPHLEGOEBQ'),
+     secret_access_key: ENV.fetch('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'),
+     s3_region: ENV.fetch('US'),
   }
 }
 end
